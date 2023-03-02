855e64fef9e1654a5acbb9b0efd111eb70a67745
Julissa Bonilla <julissab9@gmail.com>
2023-03-01 19:12:40 -0600

    updated category model

--- Develop/models/Category.js
+++ Develop/models/Category.js
@@ -4,9 +4,20 @@ const sequelize = require('../config/connection.js');
 
 class Category extends Model {}
 
+// define columns
 Category.init(
   {
-    // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    categoryName: {
+      type: DataTypes.STRING,
+      allowNull: false
+    }
+    
   },
   {
     sequelize,
